7315288909c248561c5c8f30d05e1097b114996d
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2024-02-25 22:59:04 +0000

    Added api

---
+++ api/v1/views/index.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Index file for package views"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+models = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'users': User,
+        'states': State
+        }
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def get_status():
+    """Return the server status"""
+    return jsonify(status="OK")

6069dbc4797a731be3b253999d2fd2ffb086e2d7
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2024-02-26 09:51:39 +0000

    Hopefully working

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,20 +3,28 @@
 
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
-from flask_cors import CORS
 from models import storage
 import os
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
-CORS(app, origins=["0.0.0.0"])
+app.register_blueprint(app_views)
 
-def teardown(exception):
+@app.teardown_appcontext
+def teardown(error):
     """Method to handle teardown context"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 not found errors"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,7 @@
 #!/usr/bin/python3
-"""Index file for package views"""
+"""Initialisation"""
+from flask import Blueprint
 
-from api.v1.views import app_views
-from flask import jsonify
-from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
-
-models = {
-        'amenities': Amenity,
-        'cities': City,
-        'places': Place,
-        'reviews': Review,
-        'users': User,
-        'states': State
-        }
-
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def get_status():
-    """Return the server status"""
-    return jsonify({"status": "OK"})
+from api.v1.views.index import *

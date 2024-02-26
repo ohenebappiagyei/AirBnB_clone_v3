2e40efcefe001488b2fb01105bea4a602159a51c
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2024-02-26 12:15:42 +0000

    testing

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,40 +1,33 @@
 #!/usr/bin/python3
-'''
-Createw Flask app; and register the blueprint app_views to Flask instance app.
-'''
+""" Flask Application """
 
-from os import getenv
-from flask import Flask, jsonify
-from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, render_template, make_response, jsonify
+from os import environ
 
 app = Flask(__name__)
 
-CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
-
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_engine(exception):
-    '''
-    Removes the current SQLAlchemy Session object after each request.
-    '''
+def close_storage(error):
+    """ Close Storage """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    '''
-    Return errmsg `Not Found`.
-    '''
-    response = {'error': 'Not found'}
-    return jsonify(response), 404
+    """Handles 404 not found errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+if __name__ == "__main__":
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-'''
-Create a route `/status` on the object app_views.
-'''
-
-
-from flask import jsonify
+"""First file"""
 from api.v1.views import app_views
+from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
-def api_status():
-    '''
-    Returns a JSON response for RESTful API health.
-    '''
-    response = {'status': 'OK'}
-    return jsonify(response)
+@app_views.route('/status')
+def status():
+    """Returns status"""
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'])
-def get_stats():
-    '''
-    Retrieves the number of each objects by type.
-    '''
-    stats = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
-    }
-    return jsonify(stats)
+@app_views.route('/stats')
+def total_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    cls_names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    tot_objs = {}
+    for i in range(len(classes)):
+        tot_objs[cls_names[i]] = storage.count(classes[i])
+
+    return jsonify(tot_objs)
